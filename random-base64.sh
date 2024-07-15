836b9101ed32ae6e3deff028c345981858c47c13
phyllis likimani <79490445+phyllisn-landmark@users.noreply.github.com>
2024-07-15 14:52:30 -0500

    Update random-base64.sh

--- random-base64.sh
+++ random-base64.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-head -c 32 /dev/urandom | base64 # Generates 32 bytes of random data and then encodes that data in Base64. The output is a Base64-encoded string of the random data.
+head -c 32 /dev/urandom | base64
